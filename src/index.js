25f403885777d1cffe4a3bc2c4c5587f7b2c499b
Vova <vir42@mail.ru>
2019-09-28 10:40:17 +0300

    init: task multiply

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  /*используем принцип умножения в столбик, в связи с тем, что умножение начинается с конца, перевернем для удобства число*/
+  let result = [];
+  let arrOfFirst = first.split('').reverse();
+  let arrofSecond = second.split('').reverse();
+ 
+
+  for (let i = 0; i < arrOfFirst.length; i++) {
+    for (let j = 0; j < arrofSecond.length; j++) {
+        let composition = arrOfFirst[i] * arrofSecond[j];
+
+        if(result[i + j]) {
+          result[i + j] = result[i + j] + composition;
+        } else {
+        result[i + j] = composition;
+        }
+    }
+  }
+
+  /* result[i] должна остаться одна цифра, десятки переносим в i+1 */
+  
+  for (let i = 0; i < result.length; i++) {
+    let numeral = result[i] % 10;
+    let up = Math.floor(result[i] / 10);
+    result[i] = numeral;
+
+    if(result[i + 1]) {
+      result[i + 1] += up;
+    } else {
+      if(up != 0) {
+        result[i + 1] = up
+      }
+    }
+  }
+
+ return result.reverse().join('');
+ 
 }
+
